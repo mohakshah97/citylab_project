40380d5152d6e2b092961eab0a8fb6d20a8c3abf
Mohak Shah <mohakshah16@gmail.com>
2025-02-04 03:36:46 +0000

    Adding Launch file

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -3,26 +3,26 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
 
-# def generate_launch_description():
+def generate_launch_description():
 
 
-#     my_node =  Node(
-#             package='robot_patrol',
-#             executable='patrol',
-#             output='screen',
-#             emulate_tty=True)
+    my_node =  Node(
+            package='robot_patrol',
+            executable='patrol',
+            output='screen',
+            emulate_tty=True)
 
-#     config_file = LaunchConfiguration('config_file', default='/home/user//.rviz2/cp5.rviz')
+    config_file = LaunchConfiguration('config_file', default='/home/user//.rviz2/cp5.rviz')
 
-#     rviz_node =     rviz_node = Node(
-#         package='rviz2',
-#         executable='rviz2',
-#         arguments=['-d', config_file],
-#         output='screen'
-#     ) 
-#     config_file = LaunchConfiguration('config_file', default='home/user/cp5.rviz')
+    rviz_node =     rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', config_file],
+        output='screen'
+    ) 
+    config_file = LaunchConfiguration('config_file', default='home/user/cp5.rviz')
 
-#     return LaunchDescription([
-#         my_node,
-#         rviz_node 
-#     ])
+    return LaunchDescription([
+        my_node,
+        rviz_node 
+    ])
